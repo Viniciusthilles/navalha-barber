84a557ac5c38b126c65edb9feac2b3290c2b936f
Viniciusthilles <161398610+Viniciusthilles@users.noreply.github.com>
2026-04-16 14:30:58 -0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Colocamos os valores direto aqui para não ter erro de variável vazia
 const supabaseUrl = 'https://sphqviimkctcwmbrdkaf.supabase.co'
-const supabaseAnonKey = 'sb_publishable_wsL5pdmhZow-M7plGL16rQ_QLLhSC8e'
+// Verifique se a chave abaixo está EXATAMENTE igual à do seu painel do Supabase
+const supabaseAnonKey = 'sb_publishable_wsL5pdmhZow-M7plGL16rQ_QLLhSC8e' 
 
 export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey)
